9e4565aa26a0c18a0788277ae44a5a2bf660f38a
Paige Johnson <paige.l.johns@gmail.com>
2022-08-16 21:10:16 -0700

    Updates

--- noflash.js
+++ noflash.js
@@ -1,25 +1,25 @@
 // Insert this script in your index.html right after the <body> tag.
 // This will help to prevent a flash if dark mode is the default.
 
-(function() {
+(function () {
   // Change these if you use something different in your hook.
-  var storageKey = 'darkMode';
-  var classNameDark = 'dark-mode';
-  var classNameLight = 'light-mode';
+  const storageKey = 'darkMode';
+  const classNameDark = 'dark-mode';
+  const classNameLight = 'light-mode';
 
   function setClassOnDocumentBody(darkMode) {
     document.body.classList.add(darkMode ? classNameDark : classNameLight);
     document.body.classList.remove(darkMode ? classNameLight : classNameDark);
   }
-  
-  var preferDarkQuery = '(prefers-color-scheme: dark)';
-  var mql = window.matchMedia(preferDarkQuery);
-  var supportsColorSchemeQuery = mql.media === preferDarkQuery;
-  var localStorageTheme = null;
+
+  const preferDarkQuery = '(prefers-color-scheme: dark)';
+  const mql = window.matchMedia(preferDarkQuery);
+  const supportsColorSchemeQuery = mql.media === preferDarkQuery;
+  let localStorageTheme = null;
   try {
     localStorageTheme = localStorage.getItem(storageKey);
   } catch (err) {}
-  var localStorageExists = localStorageTheme !== null;
+  const localStorageExists = localStorageTheme !== null;
   if (localStorageExists) {
     localStorageTheme = JSON.parse(localStorageTheme);
   }
@@ -34,8 +34,7 @@
     localStorage.setItem(storageKey, mql.matches);
   } else {
     // source of truth from document.body
-    var isDarkMode = document.body.classList.contains(classNameDark);
+    const isDarkMode = document.body.classList.contains(classNameDark);
     localStorage.setItem(storageKey, JSON.stringify(isDarkMode));
   }
-})();
-
+}());
